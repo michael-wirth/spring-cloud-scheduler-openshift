a2f87a8fed03e7072a52d7002599a00d7a10810a
Michael Wirth <michael.wirth@mimacom.com>
2019-05-23 21:38:05 +0200

    fix: update to release versions

--- src/main/java/org/springframework/cloud/scheduler/spi/openshift/OpenShiftScheduler.java
+++ src/main/java/org/springframework/cloud/scheduler/spi/openshift/OpenShiftScheduler.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -97,6 +97,7 @@ public class OpenShiftScheduler extends AbstractKubernetesDeployer implements Sc
 						this.mavenProperties, (OpenShiftClient) this.client,
 						this.mavenResourceJarExtractor, this.resourceHash,
 						this.containerFactory) {
+
 					@Override
 					protected String launchDockerResource(AppDeploymentRequest request) {
 						ScheduleRequest updatedScheduleRequest = new ScheduleRequest(
